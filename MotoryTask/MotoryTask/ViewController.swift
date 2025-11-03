c6de373786da427ddd20e83a0c4e7f9ddd32e44f
Ammar Al-amm <ammar.alam@leading-point.com>
2025-11-03 07:26:26 +0300

    Initialize project structure and Xcode workspace

---
+++ MotoryTask/MotoryTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotoryTask
+//
+//  Created by Ammar Al-amm on 03/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MotoryTask/MotoryTaskTests/MotoryTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotoryTaskTests.swift
+//  MotoryTaskTests
+//
+//  Created by Ammar Al-amm on 03/11/2025.
+//
+
+import Testing
+@testable import MotoryTask
+
+struct MotoryTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
